790cc2bb39218497b21004fbf6dbeee1de6e1b50
skkim125 <kthanks125@gmail.com>
2024-06-05 14:05:43 +0900

    Initial Commit

---
+++ Assignment_240605/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_240605
+//
+//  Created by 김상규 on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
